069d6ef42e54825df8086da7ccf4fbd5e0215338
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-05-09 02:01:04 -0300

    Added Test Preference Data

---
+++ preference.py
@@ -0,0 +1,20 @@
+# SDK do Mercado Pago
+import mercadopago
+# Adicione as credenciais
+sdk = mercadopago.SDK("TEST-390474968038201-050900-8f0e58898bfd4925699bcf76e196d0bc-387339876")
+
+
+# Cria um item na preferência
+preference_data = {
+    "items": [
+        {
+            "id": "1",
+            "title": "Stardew Valley",
+            "currency_id": "BRL",
+            "unit_price": 24.99
+        }
+    ]
+}
+
+preference_response = sdk.preference().create(preference_data)
+preference = preference_response["response"]
